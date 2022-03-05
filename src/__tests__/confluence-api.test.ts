372cbe07342b4abfe42c3480f235e8cd3dcced6b
Tom Duffield <github@tomduffield.com>
2022-03-05 12:49:36 -0600

    Configure Jest with initial set of empty tests

---
+++ src/__tests__/confluence-api.test.ts
@@ -0,0 +1,15 @@
+import { ConfluenceAPI } from "../confluence-api";
+
+test.todo("getPage");
+
+test.todo("createPage");
+
+test.todo("updatePage");
+
+test.todo("uploadAttachment");
+
+describe("apiRequest", () => {
+  test.todo("success");
+
+  test.todo("failure");
+});
---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,19 @@
+import { pods } from "../index";
+
+describe("config", () => {
+  test.todo("returns PublishConfig");
+});
+
+describe("plant", () => {
+  describe("unpublished note", () => {
+    test.todo("creates empty note first");
+  });
+
+  describe("published note", () => {
+    test.todo("publishes note and updates frontmatter");
+  });
+
+  describe("note with images in assets folder", () => {
+    test.todo("uploads referenced files as attachments");
+  });
+});
