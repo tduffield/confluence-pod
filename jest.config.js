bc7d86da533f2d9be08a811a67d4af19b005459f
Tom Duffield <github@tomduffield.com>
2022-03-12 13:58:21 -0600

    Update Jest and TSConfig to match settings in Dendron
    Hopefully this will maximize compatibility between the two projects.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  clearMocks: true,
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
+  },
 };
